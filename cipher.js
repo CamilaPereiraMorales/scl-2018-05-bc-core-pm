46d89ce668aef4d759047239c6ab36812f909fb7
Camila <camipereira2111@gmail.com>
2018-05-31 22:43:57 -0400

    Algunas modificaciones en css, se agrega funcion para cifrado

--- cipher.js
+++ cipher.js
@@ -1,8 +1,31 @@
+const aAscii=65;
+const alfabet=26;
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+    encode: (text, offset) => {
+      let textoFinal="";
+      for (let index=0 ; index<text.length ; index++){
+        //obtiene el numero de una letra que está en la posición index
+        let numero = text.charCodeAt(index);
+        let textoCifrado = (numero - aAscii + offset) % alfabet + aAscii;
+        textoFinal += String.fromCharCode(textoCifrado);
+      }
+
+      return textoFinal;
+    },
+
+    decode: (text, offset) => {
+      let textoFinalDescifrado="";
+      for (let index=0 ; index<text.lenght ; index++){
+        let numero = frase.charCodeAt(index);
+        let textoDesCifrado = (numero - aAscii - offset) % alfabet + aAscii;
+        textoFinalDescifrado += String.fromCharCode(TextoDescifrado);
+      
+      }
+
+      return textoFinalDescifrado
+    
+      /* Acá va tu código 
+      frase=documentgetElementById("text").value;*/
+    }
   }
-}
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function cifrarClick(){
+    let frase =document.getElementById("text").value; /*Llamo al texto ingresado por el usuario */
+    frase=frase.toUpperCase();/*convierto el string ingresado a mayusculas*/
+    let offset= 2;
+    let encodeMessage="";
+    
+    document.getElementById("textoListo").innerHTML=window.cipher.encode(frase , offset);
+}
 /* Acá va tu código */
