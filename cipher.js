fd8dbb8d012c42e36b3b82e110761e2df375323f
Camila <camipereira2111@gmail.com>
2018-06-01 15:07:15 -0400

    Se agrego un README para explicar el funcionamiento de la pagina, se modifico las funciones en la parte del descifrado

--- cipher.js
+++ cipher.js
@@ -1,31 +1,28 @@
-const aAscii=65;
-const alfabet=26;
-
 window.cipher = {
-    encode: (text, offset) => {
-      let textoFinal="";
-      for (let index=0 ; index<text.length ; index++){
-        //obtiene el numero de una letra que está en la posición index
-        let numero = text.charCodeAt(index);
-        let textoCifrado = (numero - aAscii + offset) % alfabet + aAscii;
-        textoFinal += String.fromCharCode(textoCifrado);
-      }
-
-      return textoFinal;
-    },
-
-    decode: (text, offset) => {
-      let textoFinalDescifrado="";
-      for (let index=0 ; index<text.lenght ; index++){
-        let numero = frase.charCodeAt(index);
-        let textoDesCifrado = (numero - aAscii - offset) % alfabet + aAscii;
-        textoFinalDescifrado += String.fromCharCode(TextoDescifrado);
-      
-      }
-
-      return textoFinalDescifrado
-    
-      /* Acá va tu código 
-      frase=documentgetElementById("text").value;*/
+  encode: (text, offset) => {
+    let textoFinal="";
+    for (let index=0 ; index<text.length ; index++){
+      //obtiene el numero de una letra que está en la posición index
+      let numero = text.charCodeAt(index);
+      let textoCifrado = (numero - 65 + offset) % 26 + 65;
+      textoFinal += String.fromCharCode(textoCifrado);
     }
+
+    return textoFinal;
+  },
+  
+  decode: (text, offset) => {
+    let textoFinalDescifrado="";
+    for (let index=0 ; index<text.length ; index++){
+      let numero = text.charCodeAt(index);
+      let textoDescifrado = (numero + 65 - offset) % 26 + 65;
+      textoFinalDescifrado += String.fromCharCode(textoDescifrado);
+    
+    }
+
+    return textoFinalDescifrado;
+  
+    /* Acá va tu código 
+    frase=documentgetElementById("text").value;*/
   }
+}
